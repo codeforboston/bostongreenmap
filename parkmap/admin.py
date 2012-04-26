5f91ffadca54374f7e94a088856647a4277940e3
Christian Spanring <cspanring@gmail.com>
2012-04-26 08:49:50 -0400

    adds park type and owner lookups to admin

--- parkmap/admin.py
+++ parkmap/admin.py
@@ -1,6 +1,6 @@
 from django.contrib.gis import admin
 
-from parkmap.models import Facility, Neighborhood, Park, Activity, Event
+from parkmap.models import Facility, Neighborhood, Park, Activity, Event, Parktype, Parkowner
 
 
 # default GeoAdmin overloads
@@ -9,9 +9,16 @@ admin.GeoModelAdmin.default_lat = 5210000
 admin.GeoModelAdmin.default_zoom = 12
 
 
+class ParklookupAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name',]
+    list_editable = ['name',]
+
+
 #admin.site.register(Greenspace, admin.OSMGeoAdmin)
 admin.site.register(Facility, admin.OSMGeoAdmin)
 admin.site.register(Park, admin.OSMGeoAdmin)
+admin.site.register(Parktype, ParklookupAdmin)
+admin.site.register(Parkowner, ParklookupAdmin)
 admin.site.register(Neighborhood, admin.OSMGeoAdmin)
 admin.site.register(Activity)
 admin.site.register(Event)
