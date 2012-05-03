6b1401a81f0f694a2cc90bd7c73f19526300965f
Tyrel Souza <tyrel@tyrelsouza.com>
2012-05-03 10:43:20 -0700

    Made new Explore page, moved scripts to external file.

---
+++ parkmap/static/parkmap/js/filters.js
@@ -0,0 +1,20 @@
+function update_parktype(neighborhood, idname, slug_or_id){
+     //Set the first item in the dropdown box
+     var out = '<option SELECTED value="----">Select Your Type</option>';
+     $.ajax({
+         url:'/api/v1/parktype/?format=json&neighborhood='+neighborhood,
+         //probe the correct park url
+         dataType:'json',
+         success:function(json){
+             $.each(json['objects'], function(key, obj) {
+                 //check whether the value returned is supposed to be an id or a slug.
+                 var value = slug_or_id = 1 ? 'id' : 'slug';
+                 //Create the new item in the dropdown list.
+                 out+= '<option value="'+obj[value]+'">' +obj['name']+'</option>';
+             });
+             //replace the items in the dropdown list, and select the first element
+             $("#"+idname).html(out);
+             $("#"+idname).selectedIndex = 0;
+         }
+     });
+}
