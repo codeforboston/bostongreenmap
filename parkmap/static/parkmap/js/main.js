125ec64f5b170d7c64f76aa4c85b94dfe761c4af
Christian Spanring <cspanring@gmail.com>
2012-05-15 12:19:08 -0700

    adds MAPC Basemap as default map

--- parkmap/static/parkmap/js/main.js
+++ parkmap/static/parkmap/js/main.js
@@ -1,14 +1,15 @@
 $(function() {
     
-    // map
-    // var parkmap = new google.maps.Map(document.getElementById("map_canvas"));
+    // initialize map
+    var mapclayer = "basemap"
     var parkmap = new google.maps.Map(document.getElementById("map_canvas"), {
-        zoom: 13,
+        zoom: 10,
+        center: new google.maps.LatLng(42.3, -71.1),
         minZoom: 10,
         maxZoom: 16,
         mapTypeControlOptions: {
             position: google.maps.ControlPosition.TOP_RIGHT,
-            mapTypeIds: ["simple",google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.SATELLITE],
+            mapTypeIds: [mapclayer, google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.SATELLITE], //,
             style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
         },
         panControl: false,
@@ -18,96 +19,8 @@ $(function() {
         streetViewControl: false
     });
 
-    // simple map style
-    var simple_style =  [
-        {
-            featureType: "administrative",
-            elementType: "geometry",
-            stylers: [
-                { visibility: "off" }
-            ]
-        },{
-            featureType: "administrative",
-            elementType: "labels",
-            stylers: [
-                { visibility: "off" },
-                { hue: "#d70000" },
-                { lightness: 10 },
-                { saturation: -95 }
-            ]
-        },{
-            featureType: "landscape",
-            elementType: "all",
-            stylers: [
-                { visibility: "off" }
-            ]
-        },{
-            featureType: "poi",
-            elementType: "geometry",
-            stylers: [
-                { visibility: "off" }
-            ]
-        },{
-            featureType: "poi",
-            elementType: "labels",
-            stylers: [
-                { visibility: "off" },
-            ]
-        },{
-            featureType: "transit",
-            elementType: "all",
-            stylers: [
-                { visibility: "off" }
-            ]
-        },{
-            featureType: "transit.line",
-            elementType: "geometry",
-            stylers: [
-                { hue: "#ff0000" },
-                { visibility: "on" },
-                { lightness: -20 }
-            ]
-        },{
-            featureType: "road",
-            elementType: "geometry",
-            stylers: [
-                { hue: "#d70000" },
-                { visibility: "simplified" },
-                { lightness: 10 },
-                { saturation: -95 }
-            ]
-        },{
-            featureType: "road",
-            elementType: "labels",
-            stylers: [
-                { visibility: "off" }
-            ]
-        },{
-            featureType: "water",
-            elementType: "geometry",
-            stylers: [
-                { visibility: "on" },
-                { hue: "#0091ff" },
-                { lightness: 30 },
-                { saturation: -100 }
-            ]
-        },{
-            featureType: "water",
-            elementType: "labels",
-            stylers: [
-                { visibility: "off" }
-            ]
-        }
-    ];
-
-    var simple_options = {
-        name: "Gray Map"
-    }
-
-    var simple = new google.maps.StyledMapType(simple_style, simple_options);
-
-    parkmap.mapTypes.set("simple", simple);
-    parkmap.setMapTypeId("simple");
+    parkmap.mapTypes.set(mapclayer, new google.maps.MAPCMapType(mapclayer));
+    parkmap.setMapTypeId(mapclayer);
 
     // encoded polylines for google maps
     var decodeLevels = function(encodedLevelsString) {
