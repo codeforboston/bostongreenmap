9184b1082e53a7d3c4a6f3e93be25a42168948e3
Christian Spanring <cspanring@gmail.com>
2013-04-15 15:15:18 -0400

    reorganizes static (assets) and media files

--- bostongreenmap/settings.py
+++ bostongreenmap/settings.py
@@ -1,9 +1,12 @@
 # Django settings for bostongreenmap project.
-import os
 
-# Defines the directory that contains the settings file as the PROJECT_ROOT
-# It is used for relative settings elsewhere.
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+from os.path import join, abspath, dirname
+
+# Defines the parent directory that contains the settings file as 
+# the PROJECT_ROOT. It is used for relative settings elsewhere.
+here = lambda *x: join(abspath(dirname(__file__)), *x)
+PROJECT_ROOT = here('..')
+root = lambda *x: join(abspath(PROJECT_ROOT), *x)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -56,7 +59,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_ROOT = root('media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -67,7 +70,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = root('collected_static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -78,7 +81,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'static'),
+    root('static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -121,7 +124,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'templates'),
+    root('templates'),
 )
 
 INSTALLED_APPS = (
