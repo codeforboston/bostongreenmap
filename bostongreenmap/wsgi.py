e92e16b76ed4678d8d28e8cff6d3c6c5d23d9e69
Christian Spanring <cspanring@gmail.com>
2013-04-29 16:13:17 -0400

    Updates wsgi.py to new settings file

--- bostongreenmap/wsgi.py
+++ bostongreenmap/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bostonparks.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bostongreenmap.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
