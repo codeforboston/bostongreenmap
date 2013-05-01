7a3de4aee181872fc2a7839147ab956c0724569d
Christian Spanring <cspanring@gmail.com>
2013-05-01 13:43:02 -0400

    Use MAPC basemap instead of parkmap in geo admin

--- templates/gis/admin/osm.js
+++ templates/gis/admin/osm.js
@@ -2,7 +2,7 @@
 {% block base_layer %}new OpenLayers.Layer.OSM("OpenStreetMap (Mapnik)");{% endblock %}
 
 {% block extra_layers %}
-var parkmap = new OpenLayers.Layer.MAPC("parkmap");
+var parkmap = new OpenLayers.Layer.MAPC("basemap");
 var aerial = new OpenLayers.Layer.Bing({
     name: "Bing Aerial",
     key: "An8pfp-PjegjSInpD2JyXw5gMufAZBvZ_q3cbJb-kWiZ1H55gpJbxndbFHPsO_HN",
