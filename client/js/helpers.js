f482f6b609a77112f858691edf02187eeaacb8e8
Bruce Garro <brucegarro@gmail.com>
2014-07-20 12:21:05 -0400

    moved carousel js into helpers.js

--- client/js/helpers.js
+++ client/js/helpers.js
@@ -3,6 +3,22 @@ define(['underscore', 'handlebars'], function(_, Handlebars) {
     var helpers = {
     	jsonify: function(objectRef) {
         	return JSON.stringify(objectRef, false, '    ');
+        },
+        sectionIntoThrees: function(responses) {
+          var countElements = 0;
+          var icons = [{images: []}];
+          responses.forEach(function(item) {
+            if (countElements == 3) {
+              icons.push({images: []});
+              countElements = 0;
+            }
+            if (item.images) {
+              icons[icons.length - 1]['images'].push({ src: images[0].src, caption: images[0].caption, name: item.name, link: item.url});
+              countElements++;
+            }
+          });
+          icons[0].activeClass = 'active';
+          return icons;
         }
     };
     return {
