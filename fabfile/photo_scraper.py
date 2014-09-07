cba57f2e24dd68842f18081717e96b26561e5ebe
Bruce Garro <brucegarro@gmail.com>
2014-09-07 15:42:42 -0400

    Setting up environment context for adding photos to db

--- fabfile/photo_scraper.py
+++ fabfile/photo_scraper.py
@@ -1,8 +1,32 @@
 from fabric.api import cd, run, put, env, task
-from fabric.operations import sudo
 import dbm
 import csv
 import os
+
+from fabric.contrib import django
+django.project('bostongreenmap')
+# from bostongreenmap.parks import models
+
+# from ..parks.models import Park
+
+CSV_FILE_PATH = os.path.join(
+	os.path.dirname(__file__),
+	"../media/curated_parkimages/park_photos_final.csv"
+)
+
+PROJECT_ROOT = "/home/bruce/repos/bruce-bostongreenmap"
+CURATE_PATH = PROJECT_ROOT+"/media/curated_parkimages"
+# @task
+# def write_curated_images_to_models():
+# 	park_info = curated_park_info()
+# 	count = 0
+# 	for url, park_id, is_curated:
+# 		# get the park
+# 		try:
+# 			park = Park.get(id=)
+# 		# if you have a park, create a new Parkimage and save it
+
+# 		# add that to the Park
 
 def get(url):
 	run("wget '{}'".format(url))
@@ -11,10 +35,7 @@ def get(url):
 	return picture_name
 
 def curated_park_info():
-	file_path = os.path.join(
-		os.path.dirname(__file__),
-		'../media/curated_parkimages/park_photos_final.csv'
-	)
+	file_path = PROJECT_ROOT+ '/media/curated_parkimages/park_photos_final.csv'
 	park_info = []
 	count = 0
 	with open(file_path) as park_csv:
@@ -26,6 +47,7 @@ def curated_park_info():
 
 			row = tuple(raw)
 			url, park_id, is_curated = row[21], int(float(row[23])), bool(row[36])
+
 			if is_curated: 
 				park_info.append( (url, park_id ) )
 			count += 1
@@ -51,7 +73,24 @@ def download_photos():
 			# print "%s in dbm: %s" % (key, key in db)
 	print "Files Downloaded: %s" % download_count
 	print "Files Skipped: %s" % skipped_count
-	for url in ['https://farm4.staticflickr.com/3709/14292507832_6dcee9cfca_b.jpg',
-					'https://farm8.staticflickr.com/7119/6942731872_7a8849e263_b.jpg']:
-		print "filesystem name for %s: %s" % (url, db[url])
 	db.close()
+
+@task
+def write_photos_to_filesystem():
+	park_info = curated_park_info()
+	db = dbm.open('has_been_downloaded', 'r')
+	for url, park_id in park_info:
+		if url in db[url]:
+			photo_path = db[url]
+			print url, photo_path
+
+
+@task
+def add_photos_to_db():
+	print CURATE_PATH+"/has_been_downloaded"
+	db = dbm.open(CURATE_PATH+'/has_been_downloaded', 'r')
+	
+	with cd(env.code+'/..'):
+		for url, park_id in curated_park_info():
+			if url in db:
+				print url, park_id
